d7d164262a9ba5141f5ab4772017f4e1877f166e
Nilton Freire <niltonff@hotmail.com>
2025-04-23 21:41:32 -0300

    Adicionado funcoes de listas de tarefa e estilizacao de cards

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ function fecharModal() {
   overlay.classList.remove("active");
   criarTarefa.classList.remove("active");
 }
+function buscarTarefa() {
+  fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+      inserirTarefa(res)
+    })
+} buscarTarefa();
+
+function inserirTarefa(listaDeTarefas) {
+    if(listaDeTarefas.length > 0) {
+     listaDeTarefas.map(tarefa => {
+        lista.innerHTML += `
+        <li>
+            <h5>${tarefa.titulo}</h5>
+            <p>${tarefa.descricao}</p>
+            <div class="actions">
+                <box-icon name='trash' type='solid' size='sm' ></box-icon>
+            </div>
+        </li>
+        `
+     })   
+    }
+}
